b188ae80edaddd6024101b557b049544488e223c
apetri <apetri@phys.columbia.edu>
2016-06-06 21:50:31 -0400

    started implementation of cutplanesTFR

--- batchcode/library/cutplanesTFR.py
+++ batchcode/library/cutplanesTFR.py
@@ -13,9 +13,11 @@ from lenstools.pipeline.simulation import SimulationBatch
 from lenstools.pipeline.settings import PlaneSettings
 from lenstools.simulations import PotentialPlane
 
-from lenstools.utils import MPIWhirlPool
+from lenstools.utils.misc import ApproxDict
+
 
 import numpy as np
+import astropy.units as u
 
 #Orchestra director of the execution
 def PlanesTFRExecution():
@@ -28,7 +30,17 @@ def PlanesTFRExecution():
 
 #Bucketing function
 def bucket(z,sorted_z):
-	pass
+	
+	if z<sorted_z[0]:
+		return (0,)
+
+	if z>sorted_z[-1]:
+		return (len(sorted_z)-1,)
+
+	for n in range(len(sorted_z)-1):
+		if (z>=sorted_z[n]) and z<sorted_z[n+1]:
+			return (n,n+1)
+
 
 #######################################################
 ################Main execution#########################
@@ -37,7 +49,7 @@ def bucket(z,sorted_z):
 def planesTFR(pool,batch,settings,node_id):
 
 	#Safety check
-	assert isinstance(pool,MPIWhirlPool) or (pool is None)
+	assert pool is None,"planesTFR must run on one processon only!"
 	assert isinstance(batch,SimulationBatch)
 	assert isinstance(settings,PlaneSettingsTFR)
 
@@ -53,7 +65,113 @@ def planesTFR(pool,batch,settings,node_id):
 	#Instantiate the appropriate SimulationIC,SimulationPlanes objects 
 	collection = model.getCollection(geometry_id)
 	realization = collection.getRealization(ic)
+	
+	#Original plane set and target plane set
 	plane_set = realization.getPlaneSet(settings.directory_name)
+	target_plane_set = realization.getPlaneSet(settings.target_plane_set)
+
+	#Read in the info file, that contains the number of the snapshot, redshift and comoving distance
+	info_filename = os.path.join(plane_set.storage,"info.txt")
+	logdriver.info("Reading plane set {0} info file at {1}".format(plane_set.name,info_filename))
+	
+	with open(info_filename,"r") as fp:
+
+		snapshot_number = list()
+		distance = list()
+		redshift = list()
+
+		#Parse each line
+		while True:
+			
+			#Split the line in snapshot,distance,redshift
+			line = fp.readline().rstrip("\n").split(",")
+			snapshot_number.append(int(line[0].split("=")[1]))
+		
+			d,unit = line[1].split("=")[1].split(" ")
+			if unit=="Mpc/h":
+				distance.append(float(d)*model.Mpc_over_h)
+			else:
+				distance.append(float(d)*getattr(u,"unit"))
+
+			redshift.append(float(line[2].split("=")[1]))
+
+		#Convert into arrays
+		snapshot_number = np.array(snapshot_number)
+		distance = u.quantity.Quantity(distance)
+		redshift = np.array(redshift)
+
+		#Sort
+		distance = distance[np.argsort(redshift)]
+		snapshot_number = snapshot_number[np.argsort(redshift)]
+		redshift = np.sort(redshift)
+
+	#Parse the cur2target mapping
+	cur2target_filename = os.path.join(target_plane_set.home,settings.cur2target)
+	logdriver.info("Reading redshift mapping from {0}".format(cur2target_filename))
+	with open(cur2target_filename,"r") as fp:
+		cur2target_parsed = json.load(fp)
+		cur2target = ApproxDict((float(z),cur2target_parsed[z]) for z in cur2target)
+
+	#Cycle over planes: for each plane, interpolate its values to the target redshift
+	info_filename = os.path.join(target_plane_set.storage,"info.txt")
+	logdriver.info("Writing target plane set {0} info file to {1}".format(plane_set.name,info_filename))
+	with open(info_filename,"w") as fp:
+
+		for n,nsnap in enumerate(snapshot_number):
+			
+			#Log
+			target_redshift = cur2target[redshift[n]]
+			logdriver.info("Redshift {0:.4f} will be mapped to redshift {1:.4f}".format(redshift[n],target_redshift))
+
+			#Write corresponding line in the info file
+			fp.write("s={0},d={1},z={2}\n".format(nsnap,distance[n],target_redshift))
+
+			#Find the correct bucket
+			z_indices = bucket(target_redshift,redshift)
+
+			#Interpolate each cut point and normal
+			for ncut,cut in settings.cut_points:
+				for normal in settings.normals:
+
+					target_plane_name = os.path.join(target_plane_set.storage,settings.name_format.format(nsnap,settings.kind,ncut,normal,settings.format))
+
+					if len(z_indices)==2:
+
+						#If inside the interpolation range, interpolate lensing density linearly
+						source_plane_name1 = os.path.join(plane_set.storage,settings.name_format.format(snapshot_number[z_indices[0]],settings.kind,ncut,normal,settings.format))
+						source_plane_name2 = os.path.join(plane_set.storage,settings.name_format.format(snapshot_number[z_indices[1]],settings.kind,ncut,normal,settings.format))
+						
+						logdriver.info("Reading plane at {0}".format(source_plane_name1))
+						source_plane1 = PotentialPlane.load(source_plane_name1)
+						logdriver.info("Reading plane at {0}".format(source_plane_name2))
+						source_plane2 = PotentialPlane.load(source_plane_name2)
+
+						#Interpolate to target redshift
+						logdriver.info("Interpolating target redshift {0} between {1}-{2}".format(target_redshift,source_plane1.redshift,source_plane2.redshift))
+
+						#TODO: Perform linear interpolation
+
+					
+					elif len(z_indices)==1:
+
+						#If out of the interpolation range, do nothing
+						source_plane_name = os.path.join(plane_set.storage,settings.name_format.format(snapshot_number[z_indices[0]],settings.kind,ncut,normal,settings.format)) 
+						logdriver.info("Target redshift {0} is out of interpolation range, plane is left unaltered")
+						logdriver.info("Reading plane at {0}".format(source_plane_name))
+						source_plane1 = PotentialPlane.load(source_plane_name)
+						source_plane1.redshift = target_redshift
+
+					else:
+						raise ValueError
+
+
+					#TODO: Apply scale factor scaling in case we are doing growth case
+
+
+					#Save to disk
+					logdriver.info("Saving scaled plane to {0}".format(target_plane_name))
+					source_plane1.save(target_plane_name)
+
 
 	#Done
 	if pool is None or pool.is_master():
@@ -65,4 +183,21 @@ def planesTFR(pool,batch,settings,node_id):
 #######################################################
 
 class PlaneSettingsTFR(PlaneSettings):
-	pass
+	
+	_section = "PlaneSettingsTFR"
+
+	@classmethod
+	def get(cls,options):
+
+		#Section name
+		section = cls._section
+		
+		#Parent constructor + additional attributes
+		settings = super(PlaneSettingsTFR,cls).get(options)
+		settings.target_plane_set = options.get(section,"target_plane_set")
+		settings.cur2target = options.get(section,"cur2target")
+		settings.with_scale_factor = options.getboolean(section,"with_scale_factor")
+
+		#Return to user
+		return settings
+
